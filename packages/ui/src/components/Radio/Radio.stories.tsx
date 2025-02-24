9dd30c899957475c7e96a55abaea8cadf592c5f5
yjsun <yjsun@pluxity.com>
2025-02-24 17:18:39 +0900

    radioGroup생성 및 추가 수정

--- packages/ui/src/components/Radio/Radio.stories.tsx
+++ packages/ui/src/components/Radio/Radio.stories.tsx
@@ -1,45 +1,65 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Radio from './Radio';
+import { RadioGroup, RadioGroupItem } from './Radio';
 
-const meta: Meta<typeof Radio > = {
-    title: 'Components/Radio',
-    component: Radio,
-    tags: ['autodocs'],
-    argTypes:{
-        variant:{
-            control: 'select',
-            options: ['primary', 'secondary']
-        },
-        size:{
-            control: 'select',
-            options: ['small', 'medium', 'large']
-        },
-        disabled:{
-            boolean: false,
-        }
-    }
-
-}
+const meta: Meta<typeof RadioGroup> = {
+  title: 'Components/Radio',
+  component: RadioGroup,
+  tags: ['autodocs'],
+};
 
 export default meta;
 
-type Story = StoryObj<typeof Radio>;
+type Story = StoryObj<typeof RadioGroup>;
+export const Primary: Story = {
+    args: {
+      children: (
+        <>
+          <RadioGroupItem value="primary01" name="primary" variant="primary" size="small">
+            Option small 01
+          </RadioGroupItem>
+          <RadioGroupItem value="primary02" name="primary" variant="primary" size="small">
+            Option small 02
+          </RadioGroupItem>
+          <RadioGroupItem value="primary03" name="primary" variant="primary" size="small">
+            Option small 03
+          </RadioGroupItem>
+        </>
+      ),
+    },
+  };
 
-export const Default: Story = {
-    args:{
-        variant: 'primary',
-        size: 'small',
-        value: 'radio',
-        children: 'radio'
-    }
+export const Secondary: Story = {
+    args: {
+      children: (
+        <>
+          <RadioGroupItem value="secondary01" name="secondary" variant="secondary" size="medium">
+            Option medium 01
+          </RadioGroupItem>
+          <RadioGroupItem value="secondary02" name="secondary" variant="secondary" size="medium">
+            Option medium 02
+          </RadioGroupItem>
+          <RadioGroupItem value="secondary03" name="secondary" variant="secondary" size="medium">
+            Option medium 03
+          </RadioGroupItem>
+        </>
+      ),
+    },
 }
 
 export const Disabled: Story = {
-    args:{
-        variant: 'primary',
-        size: 'small',
-        value: 'radio',
-        children: 'radio',
-        disabled: true
-    }
+    args: {
+      children: (
+        <>
+          <RadioGroupItem value="disabled01" name="disabled" variant="primary" size="large" disabled>
+            Option large 01
+          </RadioGroupItem>
+          <RadioGroupItem value="disabled02" name="disabled" variant="primary" size="large" disabled>
+            Option large 02
+          </RadioGroupItem>
+          <RadioGroupItem value="disabled03" name="disabled" variant="primary" size="large" disabled>
+            Option large 03
+          </RadioGroupItem>
+        </>
+      ),
+    },
 }
--- packages/ui/src/components/Radio/Radio.tsx
+++ packages/ui/src/components/Radio/Radio.tsx
@@ -1,115 +1,173 @@
-import { useState, HTMLAttributes } from "react";
+import * as React from "react";
+import { useState, createContext, useContext, ReactNode} from "react";
 import { cn } from "../../utils/classname";
 
-export interface RadioProps extends HTMLAttributes<HTMLInputElement> {
-  variant?: "primary" | "secondary";
-  size?: "small" | "medium" | "large";
-  name?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string;
-  disabled?: boolean;
+interface RadioGroupContextProps {
+  selectedValue: string | null;
+  onChange: (value: string) => void;
 }
 
-const Radio = ({
-  variant = "primary",
-  size = "small",
-  disabled = false,
-  name,
-  value,
-  onChange,
-  children,
-  className,
-  ...props
-}: RadioProps) => {
-  // 라디오버튼 input 스타일
-  const inputStyle =
-    "inline-block border-1 rounded-full relative after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2 after:w-1/2 after:h-1/2 after:rounded-full after:bg-white after:border-full after:transform";
+const RadioGroupContext = createContext<RadioGroupContextProps | undefined>(undefined);
 
-  const inputSizeStyle = {
-    small: "w-4 h-4",
-    medium: "w-5 h-5",
-    large: "w-6 h-6",
-  }[size];
+interface RadioGroupProps {
+  value?: string; 
+  defaultValue?: string; 
+  onChange?: (value: string) => void;
+  children: ReactNode;
+  className?: string;
+}
 
-  const inputVariantStyle = {
-    primary: "bg-white border-black after:border-black",
-    secondary: "bg-white border-secondary-500 after:border-black",
-  }[variant];
+const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>((
+  { value, defaultValue, onChange, children, ...props },
+  ref
+) => {
+  const [internalValue, setInternalValue] = useState<string | null>(defaultValue || null);
+  const selectedValue = value !== undefined ? value : internalValue;
 
-  const inputRadioStyle = {
-    primary: "bg-primary-500 border-primary-500",
-    secondary: "bg-secondary-500 border-secondary-500",
-  }[variant];
 
-  const inputDisabledStyle = disabled
-    ? "bg-gray-200 border-gray-300 cursor-not-allowed"
-    : "";
-
-  // 라디오버튼 label 스타일
-  const labelStyle = "cursor-pointer inline-flex gap-x-1 items-center";
-  const labelSizeStyle = {
-    small: "text-sm",
-    medium: "text-base",
-    large: "text-lg",
-  }[size];
-
-  const labelVariantStyle = {
-    primary: "text-black",
-    secondary: "text-secondary-500",
-  }[variant];
-
-  const labelDisabledStyle = disabled ? "text-gray-400" : "";
-
-  // 라디오버튼 동작
-  const [checked, setChecked] = useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (newValue: string) => {
     if (onChange) {
-      onChange(e); 
-    } else {
-      setChecked(e.target.checked); 
+      onChange(newValue);
+    }
+    if (value === undefined) {
+      setInternalValue(newValue);
     }
   };
 
   return (
-    <label
-      role="radio"
-      aria-checked={checked}
-      htmlFor={value}
-      className={cn(
-        labelStyle,
-        labelSizeStyle,
-        labelVariantStyle,
-        labelDisabledStyle,
-        className
-      )}
-    >
-      <input
-        type="radio"
-        id={value}
-        name={name}
-        disabled={disabled}
-        checked={checked}
-        onChange={handleChange}
-        className="absolute opacity-0"
+    <RadioGroupContext.Provider value={{ selectedValue, onChange: handleChange }}>
+      <div
+        role="radiogroup"
+        className={cn("grid gap-2", props.className)}
+        ref={ref}
         {...props}
-      />
-      <span
-        className={cn(
-          inputStyle,
-          inputSizeStyle,
-          inputVariantStyle,
-          inputDisabledStyle,
-          checked
-            ? `after:block after:border-white ${inputRadioStyle}`
-            : `after:hidden`,
-          "transition-all duration-300"
-        )}
-      ></span>
-      {children}
-    </label>
+      >
+        {children}
+      </div>
+    </RadioGroupContext.Provider>
   );
-};
+});
 
-export default Radio;
+
+// RadioGroupItem 컴포넌트
+export interface RadioItemProps extends React.HTMLAttributes<HTMLLabelElement> {
+  variant?: "primary" | "secondary";
+  size?: "small" | "medium" | "large";
+  name?: string;
+  value?: string;
+  label?:string;
+  disabled?: boolean;
+  className?: string;
+  onChange?: () => void;
+}
+
+const RadioGroupItem = React.forwardRef<HTMLLabelElement, RadioItemProps>(
+  ({
+    variant = "primary",
+    size = "small",
+    label,
+    name,
+    value,
+    disabled = false,
+    children,
+    className,
+    ...props
+  }, ref) => {
+
+    // 라디오버튼 input 스타일
+    const inputStyle =
+      "z- inline-block border-1 rounded-full relative after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2 after:w-1/2 after:h-1/2 after:rounded-full after:bg-white after:border-full after:transform";
+
+    const inputSizeStyle = {
+      small: "w-4 h-4",
+      medium: "w-5 h-5",
+      large: "w-6 h-6",
+    }[size];
+
+    const inputVariantStyle = {
+      primary: "bg-white border-black after:border-black",
+      secondary: "bg-white border-black after:border-black",
+    }[variant];
+
+    const inputRadioStyle = {
+      primary: "bg-primary-500 border-primary-500",
+      secondary: "bg-secondary-500 border-secondary-500",
+    }[variant];
+
+    const inputDisabledStyle = disabled
+      ? "bg-gray-200 border-gray-300 cursor-not-allowed"
+      : "";
+
+    // 라디오버튼 label 스타일
+    const labelStyle = "cursor-pointer inline-flex gap-x-1 items-center";
+    const labelSizeStyle = {
+      small: "text-sm",
+      medium: "text-base",
+      large: "text-lg",
+    }[size];
+
+    const labelVariantStyle = {
+      primary: "text-black",
+      secondary: "text-black",
+    }[variant];
+
+    const labelDisabledStyle = disabled ? "text-gray-400" : "";
+
+    const context = useContext(RadioGroupContext);
+    if (!context) {
+        throw new Error('RadioGroupItem은 RadioGroup 내부에서만 사용해야 합니다.');
+    }
+    const { selectedValue, onChange } = context;
+
+    const handleChange = () => {
+        if (onChange && value) {
+            onChange(value);
+        }
+    };
+
+    return (
+      <label
+        aria-checked={selectedValue === value}
+        htmlFor={value}
+        ref={ref}
+        className={cn(
+          labelStyle,
+          labelSizeStyle,
+          labelVariantStyle,
+          labelDisabledStyle,
+          className
+        )}
+        {...props}
+        >
+        <input
+          type="radio"
+          id={value}
+          className="absolute opacity-0 z-1"
+          disabled={disabled}
+          checked={selectedValue === value}
+          onChange={handleChange}
+          name={name}
+          value={value} 
+        />
+        <span
+          className={cn(
+            inputStyle,
+            inputSizeStyle,
+            inputVariantStyle,
+            inputDisabledStyle,
+            selectedValue === value
+              ? `after:block after:border-white ${inputRadioStyle}`
+              : `after:hidden`,
+            "transition-all duration-300"
+          )}
+        ></span>
+        {children}
+      </label>
+    );
+  }
+);
+
+RadioGroupItem.displayName = "RadioGroupItem";
+RadioGroup.displayName = "RadioGroup";
+
+export { RadioGroup, RadioGroupItem };
