5fdd6a4a86d31c2c8e85d0a159205ddec6fb45cc
yjsun <yjsun@pluxity.com>
2025-03-04 11:16:41 +0900

    inputList 동작 추가

--- apps/sample/src/App.tsx
+++ apps/sample/src/App.tsx
@@ -2,7 +2,8 @@ import { Button } from "@plug/ui/src/components/Button";
 import { Time } from "@plug/ui/src/components/Time";
 import { Badge } from "@plug/ui/src/components/Badge";
 import { Checkbox } from "@plug/ui/src/components/Checkbox";
-import { InputText, InputIcon} from "@plug/ui/src/components/Input";
+import { InputText, InputIcon } from "@plug/ui/src/components/Input";
+import { InputList, InputListItem } from "@plug/ui/src/components/Input/InputList";
 import { RadioGroup, RadioGroupItem } from "@plug/ui/src/components/Radio";
 import { useState, useRef } from "react";
 
@@ -17,10 +18,15 @@ function App() {
   return (
     <>
       <div className="h-screen w-screen">
+        <div className="bg-gray-400 text-sm p-2 my-2">버튼 Guide</div>
         <Button>버튼</Button>
+        <div className="bg-gray-400 text-sm p-2 my-2">시간 Guide</div>
         <Time>시간</Time>
+        <div className="bg-gray-400 text-sm p-2 my-2">뱃지 Guide</div>
         <Badge>뱃지</Badge>
+        <div className="bg-gray-400 text-sm p-2 my-2">체크박스 Guide</div>
         <Checkbox label="체크박스" variant="primary" type="circle" disabled/>
+        <div className="bg-gray-400 text-sm p-2 my-2">라디오버튼 Guide</div>
         <RadioGroup variant="primary" defaultValue="1" name="group1" onChange={(value) => { setGroup1(value); }}>
           <RadioGroupItem value="1" label="option1"/>
           <RadioGroupItem value="2" label="option2" disabled/>
@@ -29,10 +35,35 @@ function App() {
           <RadioGroupItem value="3" label="option3"/>
           <RadioGroupItem value="4" label="option4" />
         </RadioGroup>
+        <div className="bg-gray-400 text-sm p-2 my-2">Input Text Guide</div>
         <InputText variant="outline" placeholder="텍스트를 입력하세요." invalid={true} />
         <InputText variant="outline" placeholder="텍스트를 입력하세요." disabled />
+        <div className="bg-gray-400 text-sm p-2 my-2">Input Icon Guide</div>
         <InputIcon position="right" placeholder="텍스트를 입력하세요." invalid={true}/>
         <InputIcon position="right" placeholder="텍스트를 입력하세요." invalid={true} disabled={true}/>
+        <div className="bg-gray-400 text-sm p-2 my-2">Input List Guide</div>
+        <InputList>
+          <InputListItem value="선택 1번"/>
+          <InputListItem value="선택 2번" />
+          <InputListItem value="선택 3번" />
+          <InputListItem value="선택 4번" />
+          <InputListItem value="선택 5번" />
+          <InputListItem value="선택 6번" />
+        </InputList>
+        <InputList invalid={true}>
+          <InputListItem value="선택 1번"/>
+          <InputListItem value="선택 2번" />
+          <InputListItem value="선택 3번" />
+          <InputListItem value="선택 4번" />
+          <InputListItem value="선택 5번" />
+        </InputList>
+        <InputList disabled={true}>
+          <InputListItem value="선택 1번"/>
+          <InputListItem value="선택 2번" />
+          <InputListItem value="선택 3번" />
+          <InputListItem value="선택 4번" />
+          <InputListItem value="선택 5번" />
+        </InputList>
         <label>{group1}</label>
         <label>{group2}</label>
         <input ref={input_value}></input>
--- packages/ui/src/components/Input/InputList.tsx
+++ packages/ui/src/components/Input/InputList.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {useState} from "react";
 import {InputText} from "./InputText";
 import { cn } from "../../utils/classname";
 
@@ -15,6 +16,11 @@ const InputList = React.forwardRef<HTMLUListElement, ListProps>(({
     children,
     ...props
   }, ref) => {
+    
+    const [selected, setSelected] = useState("");
+    const handleClick = (value: string) => {
+      setSelected(prev => (prev === value ? "" : value));
+    }
 
     const listStyle = 'inline-flex flex-col gap-1 p-0.5 border border-1 rounded-xs overflow-y-auto min-h-41 max-h-41 min-w-55';
     const invalidStyle = invalid === true ? 'border-red-600 text-red-500' : 'border-gray-400';
@@ -23,10 +29,14 @@ const InputList = React.forwardRef<HTMLUListElement, ListProps>(({
     const listItemControl = React.Children.map(children, (child) => {
       if (React.isValidElement(child)) {
         const element = child as React.ReactElement<ListItemsProps>;
+        const value = element.props.value || "";
+
         return React.cloneElement(element, {
           ...(element.props as any),
           disabled: disabled,
           invalid: invalid,
+          selected: selected === value,
+          onClick: () => handleClick(value),
           } as any);
       }
       return child;
@@ -50,6 +60,7 @@ const InputList = React.forwardRef<HTMLUListElement, ListProps>(({
   });
 
 export interface ListItemsProps extends React.ComponentProps<'li'> {
+  selected?: boolean;
   invalid?: boolean;
   disabled?: boolean;
   value?: string;
@@ -57,22 +68,29 @@ export interface ListItemsProps extends React.ComponentProps<'li'> {
 }
 
 const InputListItem = React.forwardRef<HTMLLIElement, ListItemsProps>(({
+  selected = false,
   invalid = false,
   disabled = false,
   value,
   className,
   children,
+  onClick,
   ...props
 }, ref) => {
 
-  const listItemStyle = "hover:bg-gray-200 px-2 py-0.5";
+  const listItemStyle = "px-2 py-0.5";
+  const selectedStyle = selected ? "bg-blue-200" : "hover:bg-gray-200";
+  const listTextStyle = invalid && selected ? "text-red-600" : selected ? "text-blue-500" : "";
 
 return (
     <li
         className={cn(
             listItemStyle,
+            selectedStyle,
             className,
         )} 
+        onClick={onClick}
+        aria-selected={selected}
         aria-disabled={disabled}
         role="option"
         ref={ref}
@@ -84,7 +102,9 @@ return (
           value={value} 
           invalid={invalid} 
           disabled={disabled} 
-          readOnly/>
+          readOnly
+          className={listTextStyle}
+          />
     </li>
 );
 });
