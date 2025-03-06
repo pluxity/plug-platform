73a5ab80129561111cb4c36154d7540e3cd40e2e
yjsun <yjsun@pluxity.com>
2025-03-07 08:56:57 +0900

    불필요 속성 제거

--- packages/ui/src/components/Input/InputPassword.tsx
+++ packages/ui/src/components/Input/InputPassword.tsx
@@ -20,25 +20,22 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputTextProps>(({
     className,
     ...props
 }, ref) => {
-    const [inputHover, setInputHover] = useState(false);
     const [inputFocus, setInputFocus] = useState(false); 
     const [passwordState, setPasswordState] = useState(false);
 
     const togglePasswordIcon = () => {setPasswordState(!passwordState);};
     const PasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {onChange?.(e);};
     
-    const mouseOver = () => {setInputHover(true);};
-    const mouseOut = () => {setInputHover(false);};
     const onFocus = () => {setInputFocus(true);};
     const onBlur = () => {setInputFocus(false);};
     
-    const iconColor = invalid ? 'red' : props.disabled ? 'lightgray' : inputHover || inputFocus  ? 'black' : 'lightgray'; 
+    const iconColor = invalid ? 'red' : props.disabled ? 'lightgray' : inputFocus  ? 'black' : 'lightgray'; 
 
     const labelWrapStyle = "inline-flex items-center gap-1";
     const labelTextStyle = "text-sm";
 
     const inputWrapStyle = `${invalid === true ? "border-red-600" : props.disabled ? "border-gray-300 bg-gray-200" : "border-gray-400"} inline-flex items-center border border-1 rounded-xs h-9 w-45`;
-    const InputTextStyle = `${invalid === true ? "enabled:placeholder:text-red-600 text-red-600" : "placeholder:text-gray-300 enabled:hover:placeholder:text-black focus:placeholder:text-black text-black"} outline-none cursor-pointer text-xs placeholder:text-xs h-full p-2 disabled:text-gray-300 disabled:cursor-not-allowed`;
+    const InputTextStyle = `${invalid === true ? "enabled:placeholder:text-red-600 text-red-600" : "placeholder:text-gray-300 focus:placeholder:text-black text-black"} outline-none cursor-pointer text-xs placeholder:text-xs h-full p-2 disabled:text-gray-300 disabled:cursor-not-allowed`;
 
     const helperTextStyle = "text-xs";
 
@@ -65,9 +62,7 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputTextProps>(({
             {labelControl ? (
                 <>
                     <div
-                        className={cn(labelWrapStyle)}
-                        onMouseOver={mouseOver} 
-                        onMouseOut={mouseOut}  
+                        className={cn(labelWrapStyle)} 
                         onFocus={onFocus}  
                         onBlur={onBlur}    
                     >
@@ -108,7 +103,11 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputTextProps>(({
                 </>
             ) : (
                 <>
-                    <div className={cn(inputWrapStyle)}>
+                    <div 
+                        className={cn(inputWrapStyle)}
+                        onFocus={onFocus}  
+                        onBlur={onBlur} 
+                    >
                         {iconPosition === "leading" && <span></span>}
                         <input
                             id={inputTextId}
--- packages/ui/src/components/Input/InputText.tsx
+++ packages/ui/src/components/Input/InputText.tsx
@@ -31,23 +31,20 @@ const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(({
     ...props
 }, ref) => {
 
-    const [inputHover, setInputHover] = useState(false);
     const [inputFocus, setInputFocus] = useState(false); 
 
     const TextChange = (e: React.ChangeEvent<HTMLInputElement>) => {onChange?.(e);};
 
-    const mouseOver = () => {setInputHover(true);};
-    const mouseOut = () => {setInputHover(false);};
     const onFocus = () => {setInputFocus(true);};
     const onBlur = () => {setInputFocus(false);};
     
-    const iconColor = invalid ? 'red' : props.disabled ? 'lightgray' : inputHover || inputFocus  ? 'black' : 'lightgray'; 
+    const iconColor = invalid ? 'red' : props.disabled ? 'lightgray' : inputFocus  ? 'black' : 'lightgray'; 
 
     const labelWrapStyle = "inline-flex items-center gap-1";
     const labelTextStyle = "text-sm";
 
     const inputWrapStyle = `${invalid === true ? "border-red-600" : props.disabled ? "border-gray-300 bg-gray-200" : "border-gray-400"} inline-flex items-center border border-1 rounded-xs h-9 w-45 `;
-    const InputTextStyle = `${invalid === true ? "enabled:placeholder:text-red-600 text-red-600" : "placeholder:text-gray-300 enabled:hover:placeholder:text-black focus:placeholder:text-black text-black"} outline-none cursor-pointer text-xs placeholder:text-xs h-full p-2 disabled:text-gray-300 disabled:cursor-not-allowed`;
+    const InputTextStyle = `${invalid === true ? "enabled:placeholder:text-red-600 text-red-600" : "placeholder:text-gray-300 focus:placeholder:text-black text-black"} outline-none cursor-pointer text-xs placeholder:text-xs h-full p-2 disabled:text-gray-300 disabled:cursor-not-allowed`;
 
     const helperTextStyle = "text-xs";
 
@@ -74,8 +71,6 @@ const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(({
                 <>
                     <div
                         className={cn(labelWrapStyle)}
-                        onMouseOver={mouseOver} 
-                        onMouseOut={mouseOut}  
                         onFocus={onFocus}  
                         onBlur={onBlur}    
                     >
@@ -117,7 +112,11 @@ const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(({
                 </>
             ) : (
                 <>
-                    <div className={cn(inputWrapStyle)}>
+                    <div 
+                        className={cn(inputWrapStyle)}
+                        onFocus={onFocus}  
+                        onBlur={onBlur} 
+                    >
                         {iconPosition === "leading" && iconSvg && iconControl('leading')}
                         <input
                             id={inputTextId}
