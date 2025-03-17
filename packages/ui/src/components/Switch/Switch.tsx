7edde351eeba455967dde1703c5886db45c2a706
yjsun <yjsun@pluxity.com>
2025-03-17 16:53:19 +0900

    switch text 크기 조절

--- packages/ui/src/components/Switch/Switch.tsx
+++ packages/ui/src/components/Switch/Switch.tsx
@@ -12,6 +12,7 @@ const Switch = React.forwardRef<HTMLLabelElement, SwitchProps>(
     defaultChecked = false,
     onChange,
     className,
+    ...props
 }, ref) => {
 
     const [switchChecked, setSwitchChecked] = useState(defaultChecked);
@@ -24,22 +25,24 @@ const Switch = React.forwardRef<HTMLLabelElement, SwitchProps>(
     }
 
     const switchLabelStyle = "inline-flex items-center cursor-pointer gap-1";
+    const switchLabelText = {
+        small: "text-sm",
+        medium: "text-base",
+        large: "text-lg",
+    }[size];
 
     const switchInputWrap = "appearance-none relative border border-gray-600 rounded-full";
-
     const switchInputSize = {
         small: "w-7 h-4 before:w-3 before:h-3",
         medium: "w-9 h-5 before:w-4 before:h-4",
         large: "w-11 h-6 before:w-5 before:h-5",
     }[size];
-
-    const switchButtonStyle = "before:content-[''] before:absolute before:rounded-full before:left-[1px] before:top-[50%] before:transform before:-translate-y-1/2 before:bg-gray-600 before:transition-transform before:duration-300 before:ease-in-out"
-
     const switchInputColor = {
         primary: "checked:bg-primary-500 checked:border-primary-600",
         secondary: "checked:bg-secondary-500 checked:border-secondary-600",
     }[color];
 
+    const switchButtonStyle = "before:content-[''] before:absolute before:rounded-full before:left-[1px] before:top-[50%] before:transform before:-translate-y-1/2 before:bg-gray-600 before:transition-transform before:duration-300 before:ease-in-out"
     const switchStateStyle = `${switchChecked 
         ? "checked:before:bg-white disabled:border-gray-400 disabled:bg-gray-300 transition-all duration-300 ease-in-out before:transition-transform before:duration-300 before:ease-in-out" 
         : "disabled:opacity-50 transition-all duration-300 ease-in-out before:transition-transform before:duration-300 before:ease-in-out"} disabled:cursor-not-allowed checked:before:translate-x-full`;
@@ -67,10 +70,12 @@ const Switch = React.forwardRef<HTMLLabelElement, SwitchProps>(
                     switchStateStyle,
                     className
                 )}
+                {...props}
             />
             {label && (
                 <span 
-                    className={cn(`${disabled ? "text-gray-400 cursor-not-allowed" : "text-black"} text-sm`)}
+                
+                    className={cn(switchLabelText, `${disabled ? "text-gray-400 cursor-not-allowed" : "text-black"}`)}
                 >{label}</span>
             )}
             
