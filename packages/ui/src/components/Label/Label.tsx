354c5bdcc837f005984146f7dec3c468fdb872ef
yjsun <yjsun@pluxity.com>
2025-03-18 10:36:42 +0900

    label 컨포넌트 생성

--- packages/ui/src/components/Label/Label.tsx
+++ packages/ui/src/components/Label/Label.tsx
@@ -4,17 +4,38 @@ import type { LabelProps } from "./Label.types";
 
 const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
 ({
-    htmlFor,
+    error,
+    disabled,
+    focused,
+    required,
+    size = "medium",
     className,
     children,
+    ...props
 }, ref) => {
+
+    const labelColor = `text-black 
+    ${error && "text-red-600"} 
+    ${disabled && "text-gray-400 cursor-not-allowed"}
+    ${focused && "text-blue-600"}
+    ${required && "before:align-middle before:content-['*'] before:mr-[3px] before:text-red-600"}`;
+
+    const labelSize = {
+        small: "text-sm ", 
+        medium: "text-base",
+        large: "text-lg"
+    }[size];
 
 
     return(
         <label 
             ref={ref}
-            htmlFor={htmlFor}
-            className={cn(" ", className)}
+            className={cn(
+                labelSize,
+                labelColor,
+                className
+            )}
+            {...props}
         >
          {children}   
         </label>
--- packages/ui/src/components/Label/Label.types.ts
+++ packages/ui/src/components/Label/Label.types.ts
@@ -1,5 +1,10 @@
 
 import { LabelHTMLAttributes } from "react";
 export interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
+    error?: boolean;
+    disabled?: boolean;
+    focused?: boolean;
+    required?: boolean;
+    size?: "small" | "medium" | "large";
     className?: string;
 }
