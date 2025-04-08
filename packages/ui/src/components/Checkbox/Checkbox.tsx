be7fda80715f32300fbc846d32d6de1b33efeec5
yjsun <yjsun@pluxity.com>
2025-04-08 12:04:55 +0900

    checkbox 컴포넌트 수정

--- packages/ui/src/components/Checkbox/Checkbox.tsx
+++ packages/ui/src/components/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { cn } from "../../utils/classname";
 import type { CheckboxProps } from "./Checkbox.types";
 
@@ -8,15 +8,31 @@ const Checkbox = ({
   type = "rectangle",
   label,
   disabled = false,
+  checked,
+  onChange,
+  indeterminate = false,
+  inputClassName,
   className,
+  ref,
   ...props
 }: CheckboxProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(e.target.checked);
+    }
+  };
 
-  // 체크박스 동작
-  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    if(inputRef.current){
+      inputRef.current.indeterminate = indeterminate;
+    }
+  }, [indeterminate]);
 
   // 체크박스 input 스타일
-  const inputStyle = "inline-block cursor-pointer bg-white border-1 border-gray-500 relative after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-2/3 after:w-1/2 after:h-1/3 after:border-t-1 after:border-r-1 after:border-white after:transform after:rotate-132";
+  const inputStyle = `inline-block cursor-pointer bg-white border-1 border-gray-500 relative after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2 after:content-[''] 
+  ${indeterminate ? "after:w-1/2 after:h-[2px] after:bg-white" : "after:-translate-y-2/3 after:w-1/2 after:h-1/3 after:border-t-1 after:border-r-1 after:border-white after:rotate-132"}`;
 
   const inputTypeStyle = {
     rectangle: "rounded-sm",
@@ -52,13 +68,8 @@ const Checkbox = ({
 
   const labelDisabledStyle = disabled ? "text-gray-400 cursor-not-allowed" : "";
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(e.target.checked);
-  };
-
   return (
     <label
-      htmlFor="checkbox"
       className={cn(
         labelStyle,
         labelSizeStyle,
@@ -69,11 +80,11 @@ const Checkbox = ({
     >
       <input
         type="checkbox"
-        id="checkbox"
         className="absolute opacity-0"
         disabled={disabled}
         checked={checked}
         onChange={handleChange}
+        ref={inputRef}
         {...props}
       />
       <span
@@ -83,7 +94,8 @@ const Checkbox = ({
           inputSizeStyle,
           inputVariantStyle,
           inputDisabledStyle,
-          checked ? `after:block` : "after:hidden", "transition-all duration-300"
+          checked ? `after:block` : "after:hidden", "transition-all duration-300",
+          inputClassName,
         )}
         ></span>
         {label}
@@ -92,5 +104,4 @@ const Checkbox = ({
 };
 
 Checkbox.displayname = 'Checkbox';
-
-export default Checkbox;
+export { Checkbox };
--- packages/ui/src/components/Checkbox/Checkbox.types.ts
+++ packages/ui/src/components/Checkbox/Checkbox.types.ts
@@ -2,9 +2,12 @@ import type { Color, Size } from '../types';
 
 type CheckboxColor = Exclude<Color, 'destructive'>;
 
-export interface CheckboxProps extends Omit<React.ComponentProps<'input'>, 'size' | 'type'> {
+export interface CheckboxProps extends Omit<React.ComponentProps<'input'>, 'size' | 'type' | 'onChange'> {
     color?: CheckboxColor;
     size?: Size;
     type?: "rectangle" | "circle";
     label?: string;
+    indeterminate?: boolean;
+    inputClassName?: string;
+    onChange?: (checked: boolean) => void;
 }
