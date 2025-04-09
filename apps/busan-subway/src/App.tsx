cf59361b44c46a3a4d1f957938c025291b18ef0c
Na <nadk@pluxity.com>
2025-04-09 11:06:22 +0900

    Hotfix : Lint 수정

--- apps/busan-subway/src/App.tsx
+++ apps/busan-subway/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
