e65ea497292c405621d58c02ecb3309ff9946a87
seung <seung@pluxity.com>
2025-04-21 16:07:30 +0900

    refactor(component-form): Unified FormItem-based component structure and improved type stability

--- packages/ui/src/components/Checkbox/Checkbox.tsx
+++ packages/ui/src/components/Checkbox/Checkbox.tsx
@@ -24,7 +24,7 @@ const Checkbox = ({
     }
   }, [indeterminate]);
 
-  const isChecked = checked ?? (value as boolean);
+  const isChecked = checked ?? (value as boolean); // 추가됨
 
   const inputStyle = `inline-block cursor-pointer bg-white border-1 border-gray-500 relative after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2 after:content-[''] 
   ${indeterminate ? "after:w-1/2 after:h-[2px] after:bg-white" : "after:-translate-y-2/3 after:w-1/2 after:h-1/3 after:border-t-1 after:border-r-1 after:border-white after:rotate-132"}`;
