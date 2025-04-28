3fe5f7a3dc10480b068ba1d8217f303477dd07e7
seung <seung@pluxity.com>
2025-04-28 16:47:25 +0900

    Hotfix: #88 boiler plate setting - husky test setting

--- apps/v1/src/App.tsx
+++ apps/v1/src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
 function App() {
-    const a=2211334;
+    const a=22113344;
     return <RouterProvider router={router} />;
 }
 
