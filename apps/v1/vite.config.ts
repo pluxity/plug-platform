7748437c344d64e45877003d6d84224504488f59
Na <nadk@pluxity.com>
2025-04-29 14:56:19 +0900

    Hotfix : V1 에 vite.config.ts 수정

--- apps/v1/vite.config.ts
+++ apps/v1/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      include: "**/*.svg",
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
