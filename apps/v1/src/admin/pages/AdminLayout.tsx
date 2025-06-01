2550f893cf61506a37b7db060cb036ce60cff3da
seungeun <seungeun@pluxity.com>
2025-06-01 10:54:05 +0900

    feat(header): 헤어에 사용자 로직 연결

--- apps/v1/src/admin/pages/AdminLayout.tsx
+++ apps/v1/src/admin/pages/AdminLayout.tsx
@@ -1,17 +1,32 @@
+import React from 'react';
 import { Button } from '@plug/ui';
 import { Outlet } from 'react-router-dom';
-
+import {useProfileStore} from "@plug/v1/auth/controller/useProfileStore";
+import {logOut} from "@plug/v1/auth/api/auth";
 
 const AdminLayout = () => {
+    const user = useProfileStore((state) => state.user);
 
     return (
         <>
             <div className='h-screen flex flex-col overflow-hidden'>
-                <header className='h-12 flex items-center py-2 px-3 bg-blue-400'>
-                    <h1>Dashboard Image Area</h1>
-                    <div className='flex items-center ml-auto gap-4 text-white'>
-                        <span className="whitespace-nowrap">Admin 접속 중</span>
-                        <Button>로그아웃</Button>
+                <header className="h-16 relative z-10 flex items-center justify-between px-6 py-2.5 bg-primary-700 backdrop-blur-sm shadow-md text-white">
+                    <div className="flex items-center gap-2.5">
+                        <div className="flex items-center justify-center rounded-full h-10 w-10 bg-white">
+                            <div className="flex items-center justify-center rounded-full h-8 w-8 ring-2 ring-primary-500 bg-white">
+                                <img src="/assets/logo.png" height={22} width={22} alt="Logo" />
+                            </div>
+                        </div>
+                        <h1 className="text-white font-bold text-lg whitespace-nowrap">
+                            부산도시철도 사상하단선 관리자 페이지
+                        </h1>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div className="flex items-end text-end leading-tight gap-1">
+                            <p className="text-gray-100">{user?.name ? `${user.name}님` : '관리자님'}</p>
+                            <p className="text-xs text-gray-200">접속 중</p>
+                        </div>
+                        <Button className="h-9 w-24 px-4 text-sm" onClick={logOut}>로그아웃</Button>
                     </div>
                 </header>
                 <main className="h-full flex-1 flex flex-row">
