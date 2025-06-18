d46b9d9a88d03abe27c44906ca77ece68c77a581
yjsun <yjsun@pluxity.com>
2025-06-18 16:46:33 +0900

    feat(designSystem): breadcrumb  추가

---
+++ packages/ui/src/components/atom/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { 
+    Breadcrumb, 
+    BreadcrumbList, 
+    BreadcrumbItem, 
+    BreadcrumbLink, 
+    BreadcrumbPage, 
+    BreadcrumbSeparator,
+    BreadcrumbEllipsis
+} from "./Breadcrumb"
+
+const meta: Meta<typeof Breadcrumb> = {
+    title: 'Design System/BreadCrumb',
+    component: Breadcrumb,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],    
+}
+
+export default meta
+type Story = StoryObj<typeof Breadcrumb>
+
+export const Default: Story = {
+    render: () => (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">홈</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">문서</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbPage>현재 페이지</BreadcrumbPage>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
+
+export const WithEllipsis: Story = {
+    render: () => (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">홈</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbEllipsis />
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">문서</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbPage>현재 페이지</BreadcrumbPage>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
