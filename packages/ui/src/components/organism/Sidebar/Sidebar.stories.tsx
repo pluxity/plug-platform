464757ca8f50a11d63d4f5e4bbb48b6b1225c3eb
seungeun <seungeun@pluxity.com>
2025-06-20 11:52:09 +0900

    feat(designSystem): sidebar 수정

--- packages/ui/src/components/organism/Sidebar/Sidebar.stories.tsx
+++ packages/ui/src/components/organism/Sidebar/Sidebar.stories.tsx
@@ -1,172 +1,68 @@
 import { Meta, StoryObj } from "@storybook/react";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup, SidebarGroupAction,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarProvider,
-  SidebarTrigger
-} from "./Sidebar";
-import {
-  Calendar,
-  ChevronDown,
-  ChevronUp,
-  Home,
-  Inbox,
-  Plus,
-  Search,
-  Settings,
-  User2,
-} from "lucide-react";
-import { Collapsible, CollapsibleTrigger } from "@radix-ui/react-collapsible";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { DropdownMenuItem } from "../dropdownmenu";
-import { CollapsibleContent } from "../collapsible";
+import { MemoryRouter } from "react-router-dom";
+import Sidebar, { SidebarItem } from "./Sidebar";
+import { useState } from "react";
+
 const meta: Meta<typeof Sidebar> = {
-  title: 'Organism/Sidebar',
+  title: "Organism/Sidebar",
   component: Sidebar,
-}
+  tags: ["component", "sidebar"],
+};
 
 export default meta;
 
 type Story = StoryObj<typeof Sidebar>;
 
-const items = [
+const sidebarItems: SidebarItem[] = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    id: "user-management",
+    label: "사용자 관리",
+    to: "/admin",
+    depth: 1,
+    showToggle: true,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    id: "user-list",
+    label: "사용자 목록",
+    to: "/admin/users",
+    depth: 2,
+    parentId: "user-management",
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    id: "group-auth-list",
+    label: "그룹/권한 목록",
+    to: "/admin/user-groups",
+    depth: 2,
+    parentId: "user-management",
   },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-]
+];
 
-export const Default:Story = {
-  render:() => (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarHeader>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuButton>
-                    Select Workspace
-                    <ChevronDown className="ml-auto" />
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-                  <DropdownMenuItem>
-                    <span>Acme Inc</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Acme Corp.</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>Application</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <a href={item.url}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-          <SidebarGroup>
-            <SidebarGroupLabel>Application</SidebarGroupLabel>
-            <SidebarGroupAction>
-              <Plus /> <span className="sr-only">Add Project</span>
-            </SidebarGroupAction>
-            <SidebarGroupContent></SidebarGroupContent>
-          </SidebarGroup>
-          <Collapsible defaultOpen className="group/collapsible">
-            <SidebarGroup>
-              <SidebarGroupLabel asChild>
-                <CollapsibleTrigger>
-                  Help
-                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                </CollapsibleTrigger>
-              </SidebarGroupLabel>
-              <CollapsibleContent>
-                <SidebarGroupContent />
-              </CollapsibleContent>
-            </SidebarGroup>
-          </Collapsible>
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuButton>
-                    <User2 /> Username
-                    <ChevronUp className="ml-auto" />
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  side="top"
-                  className="w-[--radix-popper-anchor-width]"
-                >
-                  <DropdownMenuItem>
-                    <span>Account</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Billing</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Sign out</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
-      </Sidebar>
-      <main>
-        <SidebarTrigger />
-        children
-      </main>
-    </SidebarProvider>
-  )
+const SidebarWrapper = () => {
+  const [activeItem, setActiveItem] = useState<string | null>(null);
+  const [expandedItems, setExpandedItems] = useState<string[]>([]);
+
+  const handleClick = (id: string) => {
+    setActiveItem(id);
+  };
+
+  const handleToggle = (id: string) =>
+    setExpandedItems((prev) =>
+      prev.includes(id) ? prev.filter((v) => v !== id) : [...prev, id]
+    );
+
+  return (
+    <MemoryRouter>
+      <Sidebar
+        items={sidebarItems}
+        activeItemId={activeItem}
+        expandedItemIds={expandedItems}
+        onItemClick={handleClick}
+        onToggleExpand={handleToggle}
+      />
+    </MemoryRouter>
+  );
+};
+
+export const Default: Story = {
+  render: () => <SidebarWrapper />,
 };
