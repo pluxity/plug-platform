2d74af5178862aacfc11cce7ec0ed895f9fb55c8
나동규 <111726054+Nadk-Pluxity@users.noreply.github.com>
2025-08-05 10:01:52 +0900

    hotfix(fix) : domains/users/index.ts 업데이트

--- apps/core/src/backoffice/domains/users/index.ts
+++ apps/core/src/backoffice/domains/users/index.ts
@@ -1,3 +1,3 @@
 export { default as Role } from './page/Role';
 export { default as User } from './page/User';
-export { default as Permission } from './page/Permission';
+export { default as Permission } from './page/Permission'; 
